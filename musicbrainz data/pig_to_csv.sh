b01d3f816dbfb88bb53d565e1e774b4aa103e882
pkasela <p.kasela@campus.unimib.it>
2019-03-21 14:58:37 +0100

    Migiorato automation dello script

--- musicbrainz data/pig_to_csv.sh
+++ musicbrainz data/pig_to_csv.sh
@@ -1,13 +1,23 @@
+#[WARN] Anything created and saved either staring with `pig_' or 
+#in the results folder will be removed when the script is executed
+
+#creates a folder (if it doesn't exist [-p]) to store the results
+#note that anything you do in this folder will not be uploaded to git
+#since we included it in the .gitignore file
+mkdir -p ./demo_results
 #Delete the old result files, because PIG does not want to overwrite
 #It brings up immediately an exception
 rm -rf ./demo_results/pig_*
-rm -rf ./demo_results/results
-mkdir ./demo_results/results
+#Here we save the ``bella copia'' of the files
+mkdir -p ./demo_results/results
+rm -r ./demo_results/results/*
+
 #Was able to configure also the MapReduce PIG, but it works only
 #on a hdfs partition, later I will upload a script with the paths
 #changed to the hdfs partition, for now we have the local version
 #which actually might be better since we have low latency.
 pig -x local pig_to_csv.pig
+
 #go to artist result folder and concatenate the files
 cd ./demo_results/pig_artist
 cat .pig_header part-m-00000 part-m-00001 part-m-00002 part-m-00004 part-m-00005 > ../results/artist.tsv
