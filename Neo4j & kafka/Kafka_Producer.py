f4189f7d6fd0bafcc0296f14496a8aac1eaca509
pkasela <p.kasela@campus.unimib.it>
2019-04-07 10:08:45 +0200

    Renamed stuff so it's more readable

--- Neo4j & kafka/Kafka_Producer.py
+++ Neo4j & kafka/Kafka_Producer.py
@@ -8,22 +8,32 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-consumer_key=""
-consumer_secret=""
-access_token=""
-access_token_secret=""
+#Create a secret.json file with the twitter keys in it.
+with open("secret.json", "r") as f:
+    secret = json.load(f)
+    consumer_key=secret["CONSUMER_KEY"]
+    consumer_secret=secret["CONSUMER_SECRET"]
+
+    access_token=secret["ACCESS_TOKEN"]
+    access_token_secret=secret["ACCESS_TOKEN_SECRET"]
+
+KafkaTopic="89" #Random name, to be decided later, prbably will be tweets :D
 
 class StdOutListener(StreamListener):
     def on_status(self, status):
-        producer.send_messages("89",status._json['text'].encode('utf-8')) #Quali altri campi oltre user.screen_name, text? 
+	#Quali altri campi oltre user.screen_name, text?
+        producer.send_messages(KafkaTopic,status._json['text'].encode('utf-8'))  
         return True
     def on_error(self, status):
         print (status)
 
 kafka = KafkaClient("localhost:9092")
 producer = SimpleProducer(kafka)
-l = StdOutListener()
+
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-stream = Stream(auth, l)
-stream.filter(track="trump")
+
+myListener = StdOutListener() 
+#Why to use StdOutListner instead of Listner (Are there some benefits?)
+stream = Stream(auth, myListener)
+stream.filter(track="vaxination")
