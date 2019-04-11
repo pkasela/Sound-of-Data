c46ea17d1eb5f4d2d0b4f953626271f26bb8f8d2
pkasela <p.kasela@campus.unimib.it>
2019-04-11 22:06:52 +0200

    Still rmaining the indexes, but hving some trble \w hive config will use VM tomorrow

--- musicbrainz data/Data_Cleaning/localToHDFS.sh
+++ musicbrainz data/Data_Cleaning/localToHDFS.sh
@@ -6,17 +6,22 @@ hadoop fs -rm -r /mbdump/*
 
 echo "Creating the necessary folders"
 hadoop fs -mkdir -p /mbdump/artist
+hadoop fs -mkdir -p /mbdump/gender
 hadoop fs -mkdir -p /mbdump/release
 hadoop fs -mkdir -p /mbdump/language
 hadoop fs -mkdir -p /mbdump/label
 hadoop fs -mkdir -p /mbdump/label_type
 hadoop fs -mkdir -p /mbdump/track
+hadoop fs -mkdir -p /mbdump/artist_credit_name
+hadoop fs -mkdir -p /mbdump/release_label
 echo "Done creating the folders"
 
 echo "-------------------------------------"
 
 echo "artist.tsv --> hdfs:///mbdump/artist"
 hadoop fs -copyFromLocal artist.tsv /mbdump/artist
+echo "gender.tsv --> hdfs:///mbdump/gender"
+hadoop fs -copyFromLocal gender.tsv /mbdump/gender
 echo "release.tsv --> hdfs:///mbdump/release"
 hadoop fs -copyFromLocal release.tsv /mbdump/release
 echo "language.tsv --> hdfs:///mbdump/language"
@@ -27,3 +32,7 @@ echo "label_type.tsv --> hdfs:///mbdump/label_type"
 hadoop fs -copyFromLocal label_type.tsv /mbdump/label_type
 echo "track.tsv --> hdfs:///mbdump/track"
 hadoop fs -copyFromLocal track.tsv /mbdump/track
+echo "artist_credit_name.tsv --> hdfs:///mbdump/artist_credit_name"
+hadoop fs -copyFromLocal artist_credit_name.tsv /mbdump/artist_credit_name
+echo "release_label.tsv --> hdfs:///mbdump/release_label"
+hadoop fs -copyFromLocal release_label.tsv /mbdump/release_label
--- musicbrainz data/sql/hive.sql
+++ musicbrainz data/sql/hive.sql
@@ -1,3 +1,8 @@
+#create the metastore with:
+#schematool -initSchema -dbType derby
+#AND
+#remember to start the metastore with:
+# hive --service metastore
 CREATE DATABASE IF NOT EXISTS mbdump;
 USE mbdump;
 
@@ -33,6 +38,18 @@ CREATE TABLE artist (
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\t'
 LOCATION '/mbdump/artist';
+
+CREATE TABLE gender (
+    id                  INTEGER,
+    name                STRING,
+    parent              INTEGER, -- references gender.id
+    child_order         INTEGER,
+    description         STRING,
+    gid                 STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LOCATION '/mbdump/gender';
 
 CREATE TABLE release (
     id                  INTEGER,
@@ -118,3 +135,78 @@ CREATE TABLE track (
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\t'
 LOCATION '/mbdump/track';
+
+CREATE TABLE artist_credit_name (
+    artist_credit       INTEGER, -- PK, references artist_credit.id CASCADE
+    position            SMALLINT, -- PK
+    artist              INTEGER, -- references artist.id CASCADE
+    name                STRING,
+    join_phrase         STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LOCATION '/mbdump/artist_credit_name';
+
+CREATE TABLE release_label(
+    id                  INTEGER,
+    release             INTEGER,
+    label               INTEGER,
+    catalog_number      STRING,
+    last_updated        STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LOCATION '/mbdump/release_label';
+
+--Done with the loading stuff, now we create the node tables.
+
+CREATE TABLE artist_final AS (
+  SELECT a.id AS id,a.gid AS gid,a.name AS name, g.name AS gender,
+    a.type AS type, 'ARTIST' AS LABEL
+  FROM artist a JOIN gender g
+    ON a.gender = g.id
+);
+
+CREATE TABLE release_final AS (
+  SELECT r.id AS id, gid, name, release_group,
+    l.language AS language, 'RELEASE' AS LABEL
+  FROM release r JOIN language l
+    ON r.language = l.id
+);
+
+CREATE TABLE label_final AS (
+  SELECT l.id AS id,l.gid AS gid,l.name AS name,
+      lt.name AS type, 'LABEL' AS LABEL
+  FROM label l JOIN label_type lt
+    ON l.type = lt.id
+);
+
+CREATE TABLE track_final AS (
+  SELECT id, gid, name, length, 'TRACK' AS LABEL
+  FROM track
+);
+
+--Done with the node tables, now we create the relationship nodes
+
+CREATE TABLE artist_release AS (
+  SELECT ac.artist AS START_ID, r.id AS END_ID, 'RELEASED' AS TYPE
+  FROM (SELECT id, artist_credit FROM release) r
+    JOIN
+       (SELECT artist_credit, artist FROM artist_credit_name) ac
+    ON r.artist_credit = ac.artist_credit
+);
+
+CREATE TABLE release_label_final AS (
+  SELECT release AS START_ID, label AS END_ID, 'SPONSORED_BY' AS TYPE
+  FROM release_label
+);
+
+--This is the heavy one, very heavy
+
+CREATE TABLE release_track AS (
+  SELECT r.id AS START_ID, t.id AS END_ID, 'CONTAINS' AS TYPE
+  FROM (SELECT id, artist_credit FROM release) r
+    JOIN
+       (SELECT id, artist_credit FROM track) t
+    ON r.artist_credit=t.artist_credit
+);
