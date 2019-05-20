a28a9e968a0ec5e249eb25fad3ecccd09eef162c
pkasela <p.kasela@campus.unimib.it>
2019-05-20 13:58:19 +0200

    Updated the copy script

--- musicbrainz_data/Data_Cleaning/localToHDFS.sh
+++ musicbrainz_data/Data_Cleaning/localToHDFS.sh
@@ -2,33 +2,24 @@
 #Assuming to be in the Data Cleaning folder
 cd mbdump
 
-echo "Copying artist.tsv --> hdfs:///mbdump/artist"
-hadoop fs -copyFromLocal artist.tsv /mbdump/
-echo "Copying l_artist_label.tsv --> hdfs:///mbdump/l_artist_label"
-hadoop fs -copyFromLocal l_artist_label.tsv /mbdump/
-echo "Copying l_artist_recording.tsv --> hdfs:///mbdump/l_artist_recording"
-hadoop fs -copyFromLocal l_artist_recording.tsv /mbdump/
-echo "Copying l_artist_release.tsv --> hdfs:///mbdump/l_artist_release"
-hadoop fs -copyFromLocal l_artist_release.tsv /mbdump/
-echo "Copying l_artist_release_group.tsv --> hdfs:///mbdump/l_artist_release_group"
-hadoop fs -copyFromLocal l_artist_release_group.tsv /mbdump/
-echo "Copying l_label_recording.tsv --> hdfs:///mbdump/l_label_recording"
-hadoop fs -copyFromLocal l_label_recording.tsv /mbdump/
-echo "Copying l_label_release.tsv --> hdfs:///mbdump/l_label_release"
-hadoop fs -copyFromLocal l_label_release.tsv /mbdump/
-echo "Copying l_recording_release.tsv --> hdfs:///mbdump/l_recording_release"
-hadoop fs -copyFromLocal l_recording_release.tsv /mbdump/
-echo "Copying label.tsv --> hdfs:///mbdump/label"
-hadoop fs -copyFromLocal label.tsv /mbdump/
-echo "Copying label_type.tsv --> hdfs:///mbdump/label_type"
-hadoop fs -copyFromLocal label_type.tsv /mbdump/
-echo "Copying language.tsv --> hdfs:///mbdump/language"
-hadoop fs -copyFromLocal language.tsv /mbdump/
-echo "Copying recording.tsv --> hdfs:///mbdump/recording"
-hadoop fs -copyFromLocal recording.tsv /mbdump/
-echo "Copying release.tsv --> hdfs:///mbdump/release"
-hadoop fs -copyFromLocal release.tsv /mbdump/
-echo "Copying release_group.tsv --> hdfs:///mbdump/release_group"
-hadoop fs -copyFromLocal release_group.tsv /mbdump/
-echo "Copying release_label.tsv --> hdfs:///mbdump/release_label"
-hadoop fs -copyFromLocal release_label.tsv /mbdump/
+all_files=("artist"
+           "label"
+           "label_type"
+           "language"
+           "recording"
+           "release"
+           "release_group"
+           "l_artist_label"
+           "l_artist_recording"
+           "l_artist_release"
+           "l_artist_release_group"
+           "l_label_recording"
+           "l_recording_release"
+           "l_release_release_group"
+           "release_label")
+
+for f in ${all_files[@]}
+do
+  echo "Copying file:///$f.tsv --> hdfs:///mbdump/$f"
+  hadoop fs -copyFromLocal $f.tsv /mbdump/
+done
