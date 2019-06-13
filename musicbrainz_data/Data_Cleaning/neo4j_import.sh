79876d29bd9de7e5a32fc9cdb03bfc0e3cf7e9c7
pkasela <p.kasela@campus.unimib.it>
2019-06-13 17:32:46 +0000

    minor change

--- musicbrainz_data/Data_Cleaning/neo4j_import.sh
+++ musicbrainz_data/Data_Cleaning/neo4j_import.sh
@@ -4,7 +4,7 @@
 #main database folder is in /var/lib/neo4j/data/databases/
 cd ../demo_results/results
 neo4j-import \
-  --into SoundofData.db \
+  --into graph.db \
   --nodes artist.tsv \
   --nodes release.tsv \
   --nodes release_group.tsv \
@@ -28,15 +28,14 @@ neo4j-import \
 # Close neo4j in case it was running
 sudo systemctl stop neo4j.service
 # Create a backup file of the old db just in case
-sudo mv /var/lib/neo4j/data/databases/graph.db /var/lib/neo4j/data/databases/graph.db.bkp
+# sudo mv /var/lib/neo4j/data/databases/graph.db /var/lib/neo4j/data/databases/graph.db.bkp
 # Shift the database to the right folder
-sudo mv SoundofData.db /var/lib/neo4j/data/databases/graph.db
+sudo mv -f graph.db /var/lib/neo4j/data/databases/
 #Give the right permissions to the new database so that neo4j can be started safely
 sudo chown -R neo4j:neo4j /var/lib/neo4j/data/databases/graph.db
 
-#start neo4j again by yourself when you feel like starting it ;)
-#otherwise uncomment the following line
-#sudo systemctl start neo4j.service
+#start neo4j again
+sudo systemctl start neo4j.service
 
 #then just go on any browser with unimib VPN and simply write:
 # VM_IP:7474, it may ask id and pass (Defaults are neo4j - neo4j)
