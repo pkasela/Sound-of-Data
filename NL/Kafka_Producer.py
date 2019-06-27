9b37d7c82ec2e847a59c5596fe0e6702c5df0793
pkasela <p.kasela@campus.unimib.it>
2019-06-27 11:21:36 +0200

    Added the second pipeline for twitter

--- NL/Kafka_Producer.py
+++ NL/Kafka_Producer.py
@@ -3,11 +3,11 @@ import json
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-import botometer
+#import botometer
 import re
 import time
-import riak
-from musicbrainz_prova import get_musicbrainz_id as FunzioneMarco
+#import riak
+#from musicbrainz_prova import get_musicbrainz_id as FunzioneMarco
 from generi import get_genres
 from urllib3.exceptions import ProtocolError
 #import ipdb; #needed for debugging
@@ -16,28 +16,28 @@ from urllib3.exceptions import ProtocolError
 genre_list = get_genres()
 print('Total number of genres: ' + str(len(genre_list)))
 
-BOT_PROB = 0.9
-riak_client = riak.RiakClient()
-users = riak_client.bucket("users")
+#BOT_PROB = 0.9
+#riak_client = riak.RiakClient()
+#users = riak_client.bucket("users")
 
 
-def store_user(user):
-    value = bom.check_account(user)['scores']['universal']
-    users.new(user, data=value).store()
-    return value
+#def store_user(user):
+#    value = bom.check_account(user)['scores']['universal']
+#    users.new(user, data=value).store()
+#    return value
 
 
-def user_is_a_bot(user):
-    value = users.get(user).data
-    if not value:
-        try:
-           value = store_user(user)
-           print("User {} has a probability of {} to be a bot".format(user,
-                                                                      value))
-        except:
-           print("Ops! Something went wrong with Botometer")
-           return False
-    return value > BOT_PROB
+#def user_is_a_bot(user):
+#    value = users.get(user).data
+#    if not value:
+#        try:
+#           value = store_user(user)
+#           print("User {} has a probability of {} to be a bot".format(user,
+#                                                                      value))
+#        except:
+#           print("Ops! Something went wrong with Botometer")
+#           return False
+#    return value > BOT_PROB
 
 
 # Create a secret.json file with the twitter keys in it.
@@ -78,43 +78,40 @@ bom = botometer.Botometer(botometer_api_url=botometer_api_url,
 # whitelist = ['SENBreakfast']
 
 # Naming and initializing the Topic
-KafkaTopic = "KafkaTopic"
+KafkaTopic = "InitialTopic"
 
 
-def remove_spaces(txt):
-    return re.sub('@[A-z0-9]+','',re.sub(r"[\n\t\\]", " ", txt).replace("RT",""))
+#def remove_spaces(txt):
+#    return re.sub('@[A-z0-9]+','',re.sub(r"[\n\t\\]", " ", txt).replace("RT",""))
     #It also removes the initial part with RT(retweet) @user_retweeted,
     # since it is recognized as an enitity but is not a musical one
 
 class Listener(StreamListener):
     # Defining the function filtering tweets:
-    def tweet_preparations(self, data_):
-        data_ = data_._json
-        data = {'user': data_["user"]["screen_name"],
-                'text': remove_spaces(
-                    data_["extended_tweet"]["full_text"] if data_["truncated"]
-                    else data_["text"]),
-                'created_at': data_['created_at'],
-        }
-        if user_is_a_bot(data['user']):
-            return "".encode("utf-8")
-        else:
-            try:
-            	data = FunzioneMarco(data)
-            except:
-                data['genres']=[]
-            if len(data['genres']) > 0:
-                return json.dumps(data).encode("utf-8")
-            else:
-                print("Tweet '" + data_["text"] +
-                      "' does not actually talk about music.")
-                return "".encode("utf-8")
+#    def tweet_preparations(self, data_):
+#        data_ = data_._json
+#        data = {'user': data_["user"]["screen_name"],
+#                'text': remove_spaces(
+#                    data_["extended_tweet"]["full_text"] if data_["truncated"]
+#                    else data_["text"]),
+#                'created_at': data_['created_at'],
+#        }
+#        if user_is_a_bot(data['user']):
+#            return "".encode("utf-8")
+#        else:
+#            try:
+#            	data = FunzioneMarco(data)
+#            except:
+#                data['genres']=[]
+#            if len(data['genres']) > 0:
+#                return json.dumps(data).encode("utf-8")
+#            else:
+#                print("Tweet '" + data_["text"] +
+#                      "' does not actually talk about music.")
+#                return "".encode("utf-8")
 
     def on_status(self, data):
-        data = self.tweet_preparations(data)
-        if len(data.decode()) > 0:
-            print(data)
-            producer.send(KafkaTopic, data)
+        producer.send(KafkaTopic, data)
         #else:
         #    self.tweet_preparations(data)
         return True
---
+++ NL/Kafka_Producer_2.py
@@ -0,0 +1,90 @@
+from kafka import KafkaProducer, KafkaConsumer
+import time
+import riak
+from musicbrainz_prova import get_musicbrainz_id as FunzioneMarco
+import botometer
+import json
+
+with open("secret.json", "r") as f:
+    secret = json.load(f)
+    consumer_key = secret["CONSUMER_KEY"]
+    consumer_secret = secret["CONSUMER_SECRET"]
+
+    access_token = secret["ACCESS_TOKEN"]
+    access_token_secret = secret["ACCESS_TOKEN_SECRET"]
+
+    # create also this field in the secret.json
+    X_RapidAPI_Key = secret["X_RapidAPI_Key"]
+
+botometer_api_url = 'https://botometer-pro.p.mashape.com'
+mashape_key = X_RapidAPI_Key
+bom = botometer.Botometer(botometer_api_url=botometer_api_url,
+                           wait_on_ratelimit=True,
+                           mashape_key=mashape_key,
+                           **twitter_app_auth)
+
+BOT_PROB = 0.9
+riak_client = riak.RiakClient()
+users = riak_client.bucket("users")
+
+def store_user(user):
+    value = bom.check_account(user)['scores']['universal']
+    users.new(user, data=value).store()
+    return value
+
+
+def user_is_a_bot(user):
+    value = users.get(user).data
+    if not value:
+        try:
+           value = store_user(user)
+           print("User {} has a probability of {} to be a bot".format(user,
+                                                                      value))
+        except:
+           print("Ops! Something went wrong with Botometer")
+           return False
+    return value > BOT_PROB
+
+def remove_spaces(txt):
+    return re.sub('@[A-z0-9]+','',re.sub(r"[\n\t\\]", " ", txt).replace("RT",""))
+
+def tweet_preparations(data_):
+    data_ = data_._json
+    data = {'user': data_["user"]["screen_name"],
+            'text': remove_spaces(
+                data_["extended_tweet"]["full_text"] if data_["truncated"]
+                else data_["text"]),
+            'created_at': data_['created_at'],
+    }
+    if user_is_a_bot(data['user']):
+        return "".encode("utf-8")
+    else:
+        try:
+            data = FunzioneMarco(data)
+        except:
+            data['genres']=[]
+        if len(data['genres']) > 0:
+            return json.dumps(data).encode("utf-8")
+        else:
+            print("Tweet '" + data_["text"] +
+                  "' does not actually talk about music.")
+            return "".encode("utf-8")
+
+consumer =  KafkaConsumer(bootstrap_servers='sandbox-hdp.hortonworks.com:6667',
+                          auto_offset_reset='earliest',
+                          consumer_timeout_ms=1000)
+
+consumer.subscribe(['InitialTopic'])
+KafkaTopic = "KafkaTopicTrial" #for the producer
+producer = KafkaProducer(bootstrap_servers='sandbox-hdp.hortonworks.com:6667')
+
+while True:
+    try:
+        for message in consumer:
+            data=tweet_preparations(message)
+            producer.send(KafkaTopic, data)
+        time.sleep(5)
+    except:
+        print("Ops! Something went wrong")
+        time.sleep(5)
+        continue
